c26b23d0c0ff82c0f2ecc71496893c496fe85eb4
Dave Wulkan <dwulkan@hotmail.com>
2024-08-08 18:22:47 -0400

    Update esqlOCStart.bat

--- esqlOCStart.bat
+++ esqlOCStart.bat
@@ -3,12 +3,12 @@ echo on
 :: esqlOCStart.bat
 ::-------------------------------------------------------------
 
-::   cd C:\GEOS\EOS\Devel\source     <-because I'm too lazy to type this
+::   cd C:\EOS\source     <-because I'm too lazy to type this
 
-SET "OC_RUNTIME=c:\GEOS\GNUCobol\bin"
-SET "esqlOC_RUNTIME=c:\GEOS\GNUCobol\esqlOC\x64\release"
+SET "OC_RUNTIME=c:\EOS\GNUCobol\bin"
+SET "esqlOC_RUNTIME=c:\EOS\GNUCobol\esqlOC\x64\release"
 ECHO ------Update PATH One Time Only!-----
 if "%PATHUPDATED%"=="" set PATH=%OC_RUNTIME%;%esqlOC_RUNTIME%";%PATH%
 SET PATHUPDATED=1
-set "COB_LIBRARY_PATH=c:\GEOS\GNUCobol\esqlOC\win32\release;%PATH%"
-c:\GEOS\EOS\pgmexe\esqlOCStart.exe
+set "COB_LIBRARY_PATH=c:\EOS\GNUCobol\esqlOC\win32\release;%PATH%"
+c:\EOS\EOS\pgmexe\esqlOCStar.exe
